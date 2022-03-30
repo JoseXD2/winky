23dab21ab8bcd98b09fe36c67780df5043756fcd
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-30 12:34:21 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -147,7 +147,11 @@ class MainMenuState extends MusicBeatState
 			controls.setKeyboardScheme(KeyboardScheme.Duo(true), true);
 
 		changeItem();
-
+		
+                #if mobileC
+		addVirtualPad(UP_DOWN, A_B);
+		#end
+		
 		super.create();
 	}
 
